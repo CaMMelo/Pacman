0c524996c46762dc32aef2c9dd0aada139830e20
CaMMelo <cmelo.marcelo.pereira@gmail.com>
2018-05-14 16:22:49 -0300

    main maze implemented

--- globals.py
+++ globals.py
@@ -1,3 +1,5 @@
+import pygame
+
 WALL    = 1 << 0
 
 PELLET  = 1 << 0
@@ -12,3 +14,18 @@ X_DIRECTION = 1
 PLUS_WAY    = 1
 
 FONT = 'fonts/ARCADECLASSIC.TTF'
+
+WALL_SPRITES = [
+    pygame.image.load('images/wall_0000.png'),
+    pygame.image.load('images/wall_0001.png'),
+    pygame.image.load('images/wall_0002.png'),
+    pygame.image.load('images/wall_0003.png'),
+    pygame.image.load('images/wall_0004.png'),
+    pygame.image.load('images/wall_0005.png'),
+    pygame.image.load('images/wall_0006.png')
+]
+
+PELLET_SPRITES = [
+    pygame.image.load('images/pellet_0000.png'),
+    pygame.image.load('images/pellet_0001.png')
+]
